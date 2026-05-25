525d0ecadfd6eb6a82a630c7259258456cb11fa6
shikharshukla89 <39381722+shikharshukla89@users.noreply.github.com>
2026-05-25 19:40:26 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let result = "";
+        for(let str of strs) {
+            result += str.length + "#" + str
+        }
+        return result
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let result = []
+        
+        let i = 0;
+        while (i < str.length) {
+            let j = i;
+            while(str[j] !== '#') {
+                j++;
+            }
+
+            // Extract Length
+            let length = parseInt(str.substring(i, j));
+
+            // Extract actual word
+            let word = str.substring(j + 1, j + 1 + length);
+
+            result.push(word)
+
+            // Move pointer to the next encode string
+            i = j + 1 + length;
+        }
+
+        return result
+    }
+}
